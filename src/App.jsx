06eebe444b8c38c1d4120152867e919691dfc2e7
vinit chaudhary <vinitchaudhary19@gnu.ac.in>
2024-10-18 19:57:38 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,29 @@
-import React, { useEffect, useState } from "react"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-  useLocation,
-} from "react-router-dom"
-import About from "./components/About"
-import Brand from "./components/Brand"
-import Carousel from "./components/Carousel"
-import Ceo from "./components/Ceo"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Services from "./components/Services"
-import Testimonials from "./components/Testimonials"
+import Footer from "./components/Footer"
+import MainPage from "./components/MainPage"
 import Gallery from "./components/Gallery"
-
-function MainPage() {
-  return (
-    <>
-      <Carousel />
-      <About />
-      <Services />
-      <Brand />
-      <Ceo />
-      <Testimonials />
-      <Contact />
-    </>
-  )
-}
-
-function AppContent() {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [currentPath, setCurrentPath] = useState(location.pathname)
-
-  useEffect(() => {
-    const handlePopState = () => {
-      setCurrentPath(window.location.pathname)
-    }
-
-    window.addEventListener("popstate", handlePopState)
-
-    return () => {
-      window.removeEventListener("popstate", handlePopState)
-    }
-  }, [])
-
-  useEffect(() => {
-    setCurrentPath(location.pathname)
-  }, [location])
-
-  const renderContent = () => {
-    switch (currentPath) {
-      case "/":
-        return <MainPage />
-      case "/gallery":
-        return <Gallery />
-      default:
-        navigate("/")
-        return <MainPage />
-    }
-  }
-
-  return (
-    <>
-      <Navbar />
-      {renderContent()}
-      <Footer />
-    </>
-  )
-}
 
 function App() {
   return (
     <Router>
-      <AppContent />
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<MainPage />}
+        />
+        <Route
+          path="/gallery"
+          element={<Gallery />}
+        />
+        {/* Add a catch-all route */}
+        <Route
+          path="*"
+          element={<MainPage />}
+        />
+      </Routes>
+      <Footer />
     </Router>
   )
 }
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,21 @@
+import Carousel from './Carousel'
+import About from './About'
+import Services from './Services'
+import Brand from './Brand'
+import Ceo from './Ceo'
+import Testimonials from './Testimonials'
+import Contact from './Contact'
+
+export default function MainPage() {
+  return (
+    <>
+      <Carousel />
+      <About />
+      <Services />
+      <Brand />
+      <Ceo />
+      <Testimonials />
+      <Contact />
+    </>
+  )
+}
